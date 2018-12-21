c97894dcb7d70a43112be5140773466287797605
William Jelvin <33911562+WJelvin@users.noreply.github.com>
2018-12-21 20:21:28 +0100

    Added some code in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,7 @@
 package com.company;
 
 import com.company.custommeal.CustomMealBuilder;
-import com.company.mealparts.LargeFries;
-import com.company.mealparts.LeafBurger;
-import com.company.mealparts.SmallSoda;
+import com.company.mealparts.*;
 import com.company.menu.Meal;
 import com.company.menu.MealFactory;
 
@@ -14,28 +12,44 @@ public class Main {
         MealFactory mealFactory = new MealFactory();
         CustomMealBuilder customMealBuilder = new CustomMealBuilder();
 
+        //Factory för att servera färdiga Meals
         Meal whupperMeal = mealFactory.getMeal("WHUPPER");
         Meal kindaHappyMeal = mealFactory.getMeal("KINDAHAPPY");
+
+        //Builder för att bygga specialbeställa Meals
         Meal customMeal = customMealBuilder.setBurger(new LeafBurger())
                 .setSideDish(new LargeFries())
                 .setSoda(new SmallSoda()).packMeal();
 
+        Meal customMeal2 = customMealBuilder.setBurger(new TinyBurger())
+                .setSideDish(new VeggieCookie())
+                .setSoda(new SmallSoda()).packMeal();
 
+        System.out.println("En beställning av en Whupper-meal innehåller:");
         System.out.println(whupperMeal.getBurger());
         System.out.println(whupperMeal.getDrinkSize());
         System.out.println(whupperMeal.getSideDish());
 
         System.out.println();
 
+        System.out.println("En beställning av en KindaHappy-meal innehåller:");
         System.out.println(kindaHappyMeal.getBurger());
         System.out.println(kindaHappyMeal.getDrinkSize());
         System.out.println(kindaHappyMeal.getSideDish());
 
         System.out.println();
 
+        System.out.println("En specialbeställd meny som innehåller:");
         System.out.println(customMeal.getBurger());
         System.out.println(customMeal.getDrinkSize());
         System.out.println(customMeal.getSideDish());
+
+        System.out.println();
+
+        System.out.println("En specialbeställd meny som innehåller:");
+        System.out.println(customMeal2.getBurger());
+        System.out.println(customMeal2.getDrinkSize());
+        System.out.println(customMeal2.getSideDish());
 
     }
 }
